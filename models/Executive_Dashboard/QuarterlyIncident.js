9275adb21dcad31f483c2c86478237364abe792e
Haktraker <b.badr@haktraknetworks.com>
2024-08-30 17:04:35 +0300

    QuarterlyIncident

--- models/Executive_Dashboard/QuarterlyIncident.js
+++ models/Executive_Dashboard/QuarterlyIncident.js
@@ -6,6 +6,7 @@ const QuarterlyIncident = new mongoose.Schema(
       type: String,
       required: [true, "Quarterly Incident month required"],
       trim: true,
+      unique: true, // Ensure the month field is unique
     },
     score: {
       type: String,
@@ -15,6 +16,6 @@ const QuarterlyIncident = new mongoose.Schema(
   { timestamps: true }
 );
 
-QuarterlyIncident.index({ month: 1, score: 1 }, { unique: true });
+QuarterlyIncident.index({ month: 1 }, { unique: true });
 
 module.exports = mongoose.model("QuarterlyIncident", QuarterlyIncident);
