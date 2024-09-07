dc69d7dc89e7f17143e010881f5e286b58c211e3
Haktraker <b.badr@haktraknetworks.com>
2024-09-07 22:41:46 +0300

    reports services

--- services/Reports/ReportDigitalRiskIntelligenceServices.js
+++ services/Reports/ReportDigitalRiskIntelligenceServices.js
@@ -1,32 +1,38 @@
-const DigitalRiskIntelligence = require("../../models/Executive_Dashboard/DigitalRiskIntelligence");
+const ReportDigitalRiskIntelligence = require("../../models/Reports/ReportDigitalRiskIntelligence");
 const factory = require("../handlersFactory");
 
 // @desc      Create Non Compliance Gaps Overview
 // @route     POST /api/executive-dashboard/digital-risk-intelligence
 // @access    private
-exports.createDigitalRiskIntelligence = factory.create(DigitalRiskIntelligence);
+exports.createReportDigitalRiskIntelligence = factory.create(
+  ReportDigitalRiskIntelligence
+);
 
 // @desc      Get Specific Non Compliance Gaps Overview by id
 // @route     GET /api/executive-dashboard/digital-risk-intelligence/:id
 // @access    Public
-exports.getDigitalRiskIntelligence = factory.getOne(DigitalRiskIntelligence);
+exports.getReportDigitalRiskIntelligence = factory.getOne(
+  ReportDigitalRiskIntelligence
+);
 
 // @desc      Get List Of Non Compliance Gaps Overview
 // @route     GET /api/executive-dashboard/digital-risk-intelligence
 // @access    Public
-exports.getDigitalRiskIntelligences = factory.getAll(DigitalRiskIntelligence);
+exports.getReportDigitalRiskIntelligences = factory.getAll(
+  ReportDigitalRiskIntelligence
+);
 
 // @desc      Update Non Compliance Gaps Overview
 // @route     PATCH /api/executive-dashboard/digital-risk-intelligence/:id
 // @access    private
-exports.updateDigitalRiskIntelligence = factory.updateOne(
-  DigitalRiskIntelligence
+exports.updateReportDigitalRiskIntelligence = factory.updateOne(
+  ReportDigitalRiskIntelligence
 );
 
 // @desc      Delete Non Compliance Gaps Overview
 // @route     DELETE /api/executive-dashboard/digital-risk-intelligence/:id
 // @access    private
 
-exports.deleteDigitalRiskIntelligence = factory.deleteOne(
-  DigitalRiskIntelligence
+exports.deleteReportDigitalRiskIntelligence = factory.deleteOne(
+  ReportDigitalRiskIntelligence
 );
